1883088e82f4a48b9e99b97a62dcb55639aaf063
anh-khue <thailyanhkhue@gmail.com>
2018-03-12 11:09:56 +0700

    Initialize project for Branch Service. [anh-khue]

---
+++ branch-service/src/main/java/io/cobra/branchservice/BranchServiceApplication.java
@@ -0,0 +1,12 @@
+package io.cobra.branchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchServiceApplication.class, args);
+	}
+}
---
+++ branch-service/src/test/java/io/cobra/branchservice/BranchServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cobra.branchservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BranchServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
